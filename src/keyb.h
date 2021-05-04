b23fa7b5db9b81c7325fe2f3cbfed09fe99b1f1e
John Tsiombikas <nuclear@member.fsf.org>
2021-05-04 15:32:11 +0300

    changed KB_ enums, special keys were overlapping regular keys...

--- src/keyb.h
+++ src/keyb.h
@@ -26,12 +26,13 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 /* special keys */
 enum {
 	KB_ESC = 27,
+	KB_BACKSP = 127,
+
 	KB_INSERT, KB_DEL, KB_HOME, KB_END, KB_PGUP, KB_PGDN,
 	KB_LEFT, KB_RIGHT, KB_UP, KB_DOWN,
 	KB_NUM_DOT, KB_NUM_ENTER, KB_NUM_PLUS, KB_NUM_MINUS, KB_NUM_MUL, KB_NUM_DIV,
 	KB_NUM_0, KB_NUM_1, KB_NUM_2, KB_NUM_3, KB_NUM_4,
 	KB_NUM_5, KB_NUM_6, KB_NUM_7, KB_NUM_8, KB_NUM_9,
-	KB_BACKSP = 127,
 
 	KB_LALT, KB_RALT,
 	KB_LCTRL, KB_RCTRL,
